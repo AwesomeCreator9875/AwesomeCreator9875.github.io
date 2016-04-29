a502054eb34b286452dc4049d7c9589f3795719f
AwesomeCreator9875 <superhacked@inbound.plus>
2016-04-29 13:47:51 +0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,3 @@
 console.log('This would be the main JS file.');
+
+<iframe allowtransparency="true" width="485" height="402" src="//scratch.mit.edu/projects/embed/100534215/?autostart=false" frameborder="0" allowfullscreen></iframe>
